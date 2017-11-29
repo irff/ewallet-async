676978a7b6a88622a6285cbfe9b9701df269ad40
Tri Ahmad Irfan <irfan@stoqo.com>
2017-11-30 01:12:54 +0700

    Init publisher

--- publisher.py
+++ publisher.py
@@ -1,31 +1,59 @@
 #!/usr/bin/env python
+import json
 import pika
+import schedule
+import threading
+import time
+from datetime import datetime
+
 
 class EWalletPublisher():
-    def __init__(self, queue_url, exchange_name):
+    def __init__(self, queue_url, exchange_name, npm):
         self.queue_url = queue_url
         self.exchange_name = exchange_name
-        self.connection = None
-        self.credentials = None
-
-    def connect(self):
-        print("queue_url={}".format(self.queue_url))
         self.credentials = pika.PlainCredentials('sisdis', 'sisdis')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.queue_url, credentials=self.credentials))
-        self.channel = self.connection.channel()
+        self.npm = npm
 
-    def publish(self, message):
-        self.channel.queue_declare(queue='queue')
-        self.channel.basic_publish(exchange=self.exchange_name,
-                                   routing_key='',
-                                   body=message)
-        self.connection.close()
-        print("Sent hello")
+    def _get_ping_message(self):
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        message = {
+            'action': 'ping',
+            'npm': self.npm,
+            'ts': timestamp
+        }
 
-    def run_ping(self):
-        pass
+        return json.dumps(message)
+
+    def _ping(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.queue_url,
+                                                                       credentials=self.credentials))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange=self.exchange_name,
+                                 exchange_type='fanout')
+
+        message = self._get_ping_message()
+        channel.basic_publish(exchange=self.exchange_name,
+                              routing_key='',
+                              body=message)
+
+        print("Sending message ({}) to exchange {}".format(message, self.exchange_name))
+
+        connection.close()
+
+    def _run_threaded(self, job_func):
+        job_thread = threading.Thread(target=job_func)
+        job_thread.start()
+
+    def publish_ping(self):
+        try:
+            schedule.every(5).seconds.do(self._run_threaded, self._ping())
+            while 1:
+                schedule.run_pending()
+                time.sleep(5)
+        except Exception as e:
+            print("Error running ping {}".format(e.message))
 
 
-ewallet_publisher = EWalletPublisher('172.17.0.3', 'EX_PINGWIN')
-ewallet_publisher.connect()
-ewallet_publisher.publish('hello world! nyem_irfan')
+ewallet_publisher = EWalletPublisher('172.17.0.3', 'EX_PING', '1306398983')
+ewallet_publisher.publish_ping()
