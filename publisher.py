7707fb2c2e9163fc3f7b8f399b9f4a8527971d23
Tri Ahmad Irfan <irfan@stoqo.com>
2017-11-29 23:17:16 +0700

    Init publisher

---
+++ publisher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import pika
+
+class EWalletPublisher():
+    def __init__(self, queue_url, exchange_name):
+        self.queue_url = queue_url
+        self.exchange_name = exchange_name
+        self.connection = None
+
+    def connect(self):
+        print("queue_url={}".format(self.queue_url))
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.queue_url))
+        self.channel = self.connection.channel()
+
+    def publish(self, message):
+        self.channel.queue_declare(queue='queue')
+        self.channel.basic_publish(exchange=self.exchange_name,
+                                   routing_key='',
+                                   body=message)
+        self.connection.close()
+        print("Sent hello")
+
+    def run_ping(self):
+        pass
+
+
+ewallet_publisher = EWalletPublisher('172.17.0.3:5672', 'EX_PINGWIN')
+ewallet_publisher.connect()
+ewallet_publisher.publish('hello world! nyem_irfan')
