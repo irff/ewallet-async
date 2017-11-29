d094dd3e4397701a7a3485346fdb3201ed909c2b
Tri Ahmad Irfan <irfan@stoqo.com>
2017-11-29 23:36:37 +0700

    Fix publisher

--- publisher.py
+++ publisher.py
@@ -6,10 +6,12 @@ class EWalletPublisher():
         self.queue_url = queue_url
         self.exchange_name = exchange_name
         self.connection = None
+        self.credentials = None
 
     def connect(self):
         print("queue_url={}".format(self.queue_url))
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.queue_url))
+        self.credentials = pika.PlainCredentials('sisdis', 'sisdis')
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.queue_url, credentials=self.credentials))
         self.channel = self.connection.channel()
 
     def publish(self, message):
@@ -24,6 +26,6 @@ class EWalletPublisher():
         pass
 
 
-ewallet_publisher = EWalletPublisher('172.17.0.3:5672', 'EX_PINGWIN')
+ewallet_publisher = EWalletPublisher('172.17.0.3', 'EX_PINGWIN')
 ewallet_publisher.connect()
 ewallet_publisher.publish('hello world! nyem_irfan')
